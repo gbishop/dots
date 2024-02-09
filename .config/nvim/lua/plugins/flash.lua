1647ee946a4ddc359ce6d60afabc7e59d4c8cbab
Gary Bishop <gary.bishop.unc@gmail.com>
2024-02-08 19:25:25 -0500

    Change flash binding

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -4,8 +4,7 @@ return {
 	opts = {},
   -- stylua: ignore
   keys = {
-    { "<cr>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<c-f>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 	lazy = false,
