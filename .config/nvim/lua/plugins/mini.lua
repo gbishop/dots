9e98015be3df6d50ce31fbe9e857938a002ed10b
Gary Bishop <gary.bishop.unc@gmail.com>
2024-02-17 17:59:54 -0500

    Switch to mini.clue

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -60,5 +60,143 @@ return {
 			remap = false,
 			desc = "S-Tab/Previous",
 		})
+
+		local miniclue = require("mini.clue")
+
+		-- Add clues for text objects so I can learn them
+		miniclue.gen_clues.text_objects = function()
+			---@type table<string, string>
+			local i = {
+				[" "] = "Whitespace",
+				['"'] = 'Balanced "',
+				["'"] = "Balanced '",
+				["`"] = "Balanced `",
+				["("] = "Balanced (",
+				[")"] = "Balanced ) including white-space",
+				[">"] = "Balanced > including white-space",
+				["<lt>"] = "Balanced <",
+				["]"] = "Balanced ] including white-space",
+				["["] = "Balanced [",
+				["}"] = "Balanced } including white-space",
+				["{"] = "Balanced {",
+				["?"] = "User Prompt",
+				_ = "Underscore",
+				a = "Argument",
+				b = "Balanced ), ], }",
+				c = "Class",
+				f = "Function",
+				o = "Block, conditional, loop",
+				q = "Quote `, \", '",
+				t = "Tag",
+			}
+			---@type table<string, string>
+			local a = vim.deepcopy(i)
+			for k, v in pairs(a) do
+				a[k] = v:gsub(" including.*", "")
+			end
+
+			-- Example of desired clues output:
+			-- { mode = "o", keys = "if", desc = "Inside Function" },
+			-- { mode = "o", keys = "af", desc = "Around Function" },
+			-- { mode = "o", keys = "inf", desc = "Inside next Function" },
+			-- { mode = "o", keys = "ilf", desc = "Inside last Function" },
+			-- { mode = "o", keys = "anf", desc = "Around next Function" },
+			-- { mode = "o", keys = "alf", desc = "Around last Function" },
+
+			local clues = {}
+			for key, name in pairs(i) do
+				table.insert(
+					clues,
+					{ mode = "o", keys = "i" .. key, desc = "Inside " .. name }
+				)
+				table.insert(
+					clues,
+					{ mode = "x", keys = "i" .. key, desc = "Inside " .. name }
+				)
+			end
+			for key, name in pairs(a) do
+				table.insert(
+					clues,
+					{ mode = "o", keys = "a" .. key, desc = "Around " .. name }
+				)
+				table.insert(
+					clues,
+					{ mode = "x", keys = "a" .. key, desc = "Around " .. name }
+				)
+			end
+
+			for key1, name1 in pairs({ n = "Next", l = "Last" }) do
+				for key2, name2 in pairs(i) do
+					-- stylua: ignore
+					table.insert(clues, { mode = "o", keys = "i" .. key1 .. key2, desc = "Inside " .. name1 .. " " .. name2 })
+				end
+				for key2, name2 in pairs(a) do
+					-- stylua: ignore
+					table.insert(clues, { mode = "o", keys = "a" .. key1 .. key2, desc = "Around " .. name1 .. " " .. name2 })
+				end
+			end
+
+			return clues
+		end
+
+		miniclue.setup({
+			triggers = {
+				-- Leader triggers
+				{ mode = "n", keys = "<Leader>" },
+				{ mode = "x", keys = "<Leader>" },
+
+				-- Built-in completion
+				{ mode = "i", keys = "<C-x>" },
+
+				-- `g` key
+				{ mode = "n", keys = "g" },
+				{ mode = "x", keys = "g" },
+
+				-- Marks
+				{ mode = "n", keys = "'" },
+				{ mode = "n", keys = "`" },
+				{ mode = "x", keys = "'" },
+				{ mode = "x", keys = "`" },
+
+				-- Registers
+				{ mode = "n", keys = '"' },
+				{ mode = "x", keys = '"' },
+				{ mode = "i", keys = "<C-r>" },
+				{ mode = "c", keys = "<C-r>" },
+
+				-- Window commands
+				{ mode = "n", keys = "<C-w>" },
+
+				-- `z` key
+				{ mode = "n", keys = "z" },
+				{ mode = "x", keys = "z" },
+
+				-- bracketed
+				{ mode = "n", keys = "[" },
+				{ mode = "n", keys = "]" },
+				{ mode = "x", keys = "[" },
+				{ mode = "x", keys = "]" },
+
+				-- text objects
+				{ mode = "o", keys = "i", desc = "inside" },
+				{ mode = "o", keys = "a", desc = "around" },
+				{ mode = "x", keys = "i", desc = "inside" },
+				{ mode = "x", keys = "a", desc = "around" },
+			},
+
+			clues = {
+				-- Enhance this by adding descriptions for <Leader> mapping groups
+				miniclue.gen_clues.builtin_completion(),
+				miniclue.gen_clues.g(),
+				miniclue.gen_clues.marks(),
+				miniclue.gen_clues.registers(),
+				miniclue.gen_clues.windows(),
+				miniclue.gen_clues.z(),
+				{ mode = "n", keys = "<Leader>l", desc = "+LSP" },
+				{ mode = "n", keys = "<Leader>d", desc = "+Debug" },
+				{ mode = "n", keys = "<Leader>s", desc = "+Search" },
+				miniclue.gen_clues.text_objects(),
+			},
+		})
 	end,
 }
