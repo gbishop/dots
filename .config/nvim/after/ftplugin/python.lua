5b1f3b8c9816a6a869f3f6dc3e55ca0216454d99
Gary Bishop <gary.bishop.unc@gmail.com>
2024-03-15 12:24:05 -0400

    Add my baking recipe script

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -11,3 +11,17 @@ vim.keymap.set(
 	"vapy:!tmux-paste-to-python<CR>",
 	{ desc = "Send chunk to python,stay", silent = true }
 )
+-- Run my bake script
+vim.keymap.set("n", "<leader>r", function()
+	local original_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+	local formatted_lines
+
+	formatted_lines = vim.fn.systemlist("python /home/gb/bake/run.py foo", original_lines)
+	if vim.v.shell_error ~= 0 then
+		error("\n\n" .. table.concat(formatted_lines, "\n") .. "\n")
+	end
+
+	local view = vim.fn.winsaveview()
+	vim.api.nvim_buf_set_lines(0, 0, -1, true, formatted_lines)
+	vim.fn.winrestview(view)
+end, { desc = "Format a recipe" })
