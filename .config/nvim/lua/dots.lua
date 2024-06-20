7ef5bb9de571f1f3e368aee6c7f6114bfaad1938
Gary Bishop <gary.bishop.unc@gmail.com>
2024-06-20 19:22:02 -0400

    WIP

--- .config/nvim/lua/dots.lua
+++ .config/nvim/lua/dots.lua
@@ -5,56 +5,49 @@ vim.keymap.set(
 	{ desc = "Save and execute" }
 )
 
-local installed_name = function(repo_name)
-	if string.match(repo_name, "/home/gb/dots/") then
-		return string.gsub(repo_name, "/dots/", "/")
-	end
-	return nil
+local Job = require("plenary.job")
+
+local sync = function()
+	Job:new({
+		command = "rsync",
+		args = {
+			"--delete",
+			"-ra",
+			"--files-from=managed.txt",
+			".",
+			"/tmp/test",
+		},
+		cwd = "/home/gb/dots",
+	}):sync()
+	print("synced")
 end
 
+local group = vim.api.nvim_create_augroup("dots", { clear = true })
+
 vim.api.nvim_create_autocmd("BufWritePost", {
-	group = vim.api.nvim_create_augroup("dots", { clear = true }),
+	group = group,
 	pattern = "/home/gb/dots/*",
-	callback = function()
-		local repo_name = vim.api.nvim_buf_get_name(0)
-		local iname = installed_name(repo_name)
-		if iname then
-			local write = "write! ++p " .. iname
-			vim.cmd(write)
-		end
-	end,
+	callback = sync,
+})
+
+vim.api.nvim_create_autocmd("User", {
+	group = group,
+	pattern = "FugitiveChanged",
+	callback = sync,
 })
 
 local nt = require("neo-tree.events")
 
 local delete_handler = {
 	event = "file_deleted",
-	handler = function(arg)
-		local iname = installed_name(arg)
-		if iname then
-			os.remove(iname)
-			print("deleted", iname)
-		end
-	end,
+	handler = sync,
 	id = 3141,
 }
 nt.subscribe(delete_handler)
 
 local rename_handler = {
 	event = "file_renamed",
-	handler = function(arg)
-		local iname = installed_name(arg.source)
-		local nname = installed_name(arg.destination)
-		if iname then
-			if nname then
-				os.rename(iname, nname)
-				print("renamed", iname, "->", nname)
-			else
-				os.remove(iname)
-				print("deleted", iname)
-			end
-		end
-	end,
+	handler = sync,
 	id = 31415,
 }
 nt.subscribe(rename_handler)
