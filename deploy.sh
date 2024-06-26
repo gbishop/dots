81ed8373084f4ee93e41829665e713ae0d44933a
Gary Bishop <gary.bishop.unc@gmail.com>
2024-06-26 10:39:21 -0400

    Clean up deleted files

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # copy the files out to where they belong
-rsync -rlt --files-from=managed.txt . $HOME
+rsync -rlt --delete --files-from=managed.txt . $HOME
 # mark them readonly to warn me not to edit them.
 # Happily rsync ignores this and copies over them just fine!
 cat managed.txt | tr '\n' '\0' | (cd $HOME; find -files0-from - -type f -exec chmod ug-w -- {} +)
