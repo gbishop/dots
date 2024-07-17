7c9c9c9ed3aa1c6491cecbf35c5d025ae6085b64
Gary Bishop <gary.bishop.unc@gmail.com>
2024-07-17 15:13:16 -0400

    Update bake script, remove from python

--- .config/nvim/after/ftplugin/bake.lua
+++ .config/nvim/after/ftplugin/bake.lua
@@ -1,11 +1,28 @@
 -- nvim/after/ftplugin/bake.lua
 --
+vim.bo.textwidth = 80
+
 -- Run my bake script
 vim.keymap.set("n", "<leader>r", function()
 	local original_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
 	local formatted_lines
 
 	formatted_lines = vim.fn.systemlist("python /home/gb/bake/bake.py", original_lines)
+	if vim.v.shell_error ~= 0 then
+		error("\n\n" .. table.concat(formatted_lines, "\n") .. "\n")
+	end
+
+	local view = vim.fn.winsaveview()
+	vim.api.nvim_buf_set_lines(0, 0, -1, true, formatted_lines)
+	vim.fn.winrestview(view)
+end, { desc = "Format a recipe" })
+
+-- Run my bake script
+vim.keymap.set("n", "<leader>R", function()
+	local original_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+	local formatted_lines
+
+	formatted_lines = vim.fn.systemlist("python /home/gb/bake/bake.py -R", original_lines)
 	if vim.v.shell_error ~= 0 then
 		error("\n\n" .. table.concat(formatted_lines, "\n") .. "\n")
 	end
