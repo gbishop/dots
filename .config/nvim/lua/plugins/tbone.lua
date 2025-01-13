abfd4d4b5f64d5f0d18d877d69326eecbb942b87
Gary Bishop <gary.bishop.unc@gmail.com>
2025-01-13 10:46:33 -0500

    Adjust python push to ipython

--- .config/nvim/lua/plugins/tbone.lua
+++ .config/nvim/lua/plugins/tbone.lua
@@ -4,13 +4,13 @@ return {
 		vim.keymap.set(
 			"n",
 			"<leader>p",
-			"v}k$:Twrite right<CR>}",
+			"vap:Twrite right<CR>}",
 			{ desc = "Send chunk to python", silent = true }
 		)
 		vim.keymap.set(
 			"n",
 			"<leader>P",
-			"v}k$:Twrite right<CR>",
+			"vap:Twrite right<CR>",
 			{ desc = "Send chunk to python,stay", silent = true }
 		)
 	end,
