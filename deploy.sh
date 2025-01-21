7c2df3fc577ea3d38f9bd7389994c16b6a4d2810
Gary Bishop <gary.bishop.unc@gmail.com>
2025-01-21 08:23:46 -0500

    Make deploy work from anywhere

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
+cwd=$(cd "${0%/*}"; pwd)
 # copy the files out to where they belong
-rsync -rlt --delete --files-from=managed.txt . $HOME
+rsync -rlt --delete --files-from=$cwd/managed.txt $cwd $HOME
 # mark them readonly to warn me not to edit them.
 # Happily rsync ignores this and copies over them just fine!
-cat managed.txt | tr '\n' '\0' | (cd $HOME; find -files0-from - -type f -exec chmod ug-w -- {} +)
+cat $cwd/managed.txt | tr '\n' '\0' | (cd $HOME; find -files0-from - -type f -exec chmod ug-w -- {} +)
