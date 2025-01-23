166efc2e252961d39e698f0be963d48cd2f6eb6c
Gary Bishop <gary.bishop.unc@gmail.com>
2025-01-23 08:22:24 -0500

    Back to cinnamon for smooth scrolling

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
 	config = function()
 		require("mini.ai").setup()
 		require("mini.align").setup({})
-		require("mini.animate").setup()
 		require("mini.bracketed").setup({})
 		require("mini.colors").setup({})
 		require("mini.indentscope").setup({})
@@ -188,18 +187,6 @@ return {
 				{ mode = "n", keys = "<Leader>d", desc = "+Debug" },
 				{ mode = "n", keys = "<Leader>s", desc = "+Search" },
 				miniclue.gen_clues.text_objects(),
-			},
-		})
-
-		local gen_loader = require("mini.snippets").gen_loader
-		require("mini.snippets").setup({
-			snippets = {
-				-- Load custom file with global snippets first (adjust for Windows)
-				gen_loader.from_file("~/.config/nvim/snippets/global.json"),
-
-				-- Load snippets based on current language by reading files from
-				-- "snippets/" subdirectories from 'runtimepath' directories.
-				gen_loader.from_lang(),
 			},
 		})
 	end,
