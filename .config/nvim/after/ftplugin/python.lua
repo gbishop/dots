0555e6257600af1cec41201a93502b44d9831db3
Gary Bishop <gary.bishop.unc@gmail.com>
2025-06-27 16:42:18 -0400

    Get pasting to python working again.
    Why did it break?

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,18 +1,18 @@
 vim.keymap.set(
   "n",
   "<leader>p",
-  "Vasgy`>/^.<cr>:silent !tmux-paste-to-python<CR>",
+  "Vas:w !tmux-paste-to-python<CR>`>/^.<cr>",
   { desc = "Send chunk to python move", silent = true, remap = true }
 )
 vim.keymap.set(
   "v",
   "<leader>p",
-  "gy`>j:silent !tmux-paste-to-python<CR>",
+  ":w !tmux-paste-to-python<CR>`>j",
   { desc = "Send selection to python move", silent = true, remap = true }
 )
 vim.keymap.set(
   "n",
   "<leader>P",
-  "Vasgy:silent !tmux-paste-to-python<CR>",
+  "Vas:w !tmux-paste-to-python<CR>",
   { desc = "Send chunk to python stay", silent = true, remap = true }
 )
