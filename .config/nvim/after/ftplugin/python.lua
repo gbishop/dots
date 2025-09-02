ed7ae420713e8df211c7dbd8dee999720d1fc9c4
Gary Bishop <gary.bishop.unc@gmail.com>
2025-09-02 08:33:51 -0400

    paste to python

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,18 +1,24 @@
 vim.keymap.set(
   "n",
   "<leader>p",
-  "Vas:w !tmux-paste-to-python<CR>`>/^.<cr>",
+  "Vas:w !tmux-paste-to-python -q<CR>`>/^.<cr>",
+  -- function()
+  --   vim.cmd("normal Vas")
+  --   local lno = vim.fn.line(".")
+  --   local command = string.format("w !tmux-paste-to-python -q %s", lno)
+  --   vim.cmd(command)
+  -- end,
   { desc = "Send chunk to python move", silent = true, remap = true }
 )
 vim.keymap.set(
   "v",
   "<leader>p",
-  ":w !tmux-paste-to-python<CR>`>j",
+  ":w !tmux-paste-to-python -q<CR>`>j",
   { desc = "Send selection to python move", silent = true, remap = true }
 )
 vim.keymap.set(
   "n",
   "<leader>P",
-  "Vas:w !tmux-paste-to-python<CR>",
+  "Vas:w !tmux-paste-to-python -q<CR>",
   { desc = "Send chunk to python stay", silent = true, remap = true }
 )
