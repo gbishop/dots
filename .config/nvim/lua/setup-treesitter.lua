c78a3d6e435b4a9695c3645d50ed1a6885938c7f
Gary Bishop <gary.bishop.unc@gmail.com>
2026-04-21 09:05:33 -0400

    Adapt treesitter setup

--- .config/nvim/lua/setup-treesitter.lua
+++ .config/nvim/lua/setup-treesitter.lua
@@ -6,9 +6,8 @@ MiniDeps.add({
     end,
   },
 })
-MiniDeps.add("nvim-treesitter/playground")
 MiniDeps.add("nvim-treesitter/nvim-treesitter-textobjects")
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter").setup({
   ensure_installed = {
     "markdown",
     "html",
@@ -86,23 +85,4 @@ require("nvim-treesitter.configs").setup({
       },
     },
   },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = "o",
-      toggle_hl_groups = "i",
-      toggle_injected_languages = "t",
-      toggle_anonymous_nodes = "a",
-      toggle_language_display = "I",
-      focus_language = "f",
-      unfocus_language = "F",
-      update = "R",
-      goto_node = "<cr>",
-      show_help = "?",
-    },
-  },
 })
-MiniDeps.add("RRethy/nvim-treesitter-textsubjects")
