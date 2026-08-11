bcca8974a4e377ed27ba708b1f27e75269e6d107
Gary Bishop <gary.bishop.unc@gmail.com>
2026-08-11 18:03:20 -0400

    Add zed

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cwd=$(cd "${0%/*}"; pwd)
 rsync -rlt --delete --files-from=$cwd/managed.txt $cwd $HOME
 # mark them readonly to warn me not to edit them.
 # Happily rsync ignores this and copies over them just fine!
-cat $cwd/managed.txt | tr '\n' '\0' | (cd $HOME; find -files0-from - -type f -exec chmod ug-w -- {} +)
+cat $cwd/managed.txt | grep -v zed | tr '\n' '\0' | (cd $HOME; find -files0-from - -type f -exec chmod ug-w -- {} +)
