732bc9289a1a7779e219b05db30cd81d4207e450
Gary Bishop <gary.bishop.unc@gmail.com>
2026-09-08 20:05:36 -0400

    Fix treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -73,7 +73,6 @@ require("setup-tmux")
 require("setup-treesitter")
 require("setup-whichkey")
 require("setup-live-preview")
-require("setup-overseer")
 
 MiniDeps.add("windwp/nvim-autopairs")
 require("nvim-autopairs").setup()
